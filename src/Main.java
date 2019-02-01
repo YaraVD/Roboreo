e3997c129cc93b332661e73faccf21fbd1aeef6d
VincentPinet <vincent.pinet@gmail.com>
2019-02-01 10:39:28 +0100

    Deplacement et capteur ultrason

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,47 @@
-import lejos.hardware.BrickFinder;
-import lejos.hardware.Button;
-import lejos.hardware.Sound;
-import lejos.hardware.lcd.Font;
-import lejos.hardware.lcd.GraphicsLCD;
-import lejos.utility.Delay;
+import lejos.hardware.motor.Motor;
+import lejos.robotics.SampleProvider;
+import lejos.robotics.navigation.MovePilot;
+import lejos.hardware.sensor.EV3UltrasonicSensor;
+import lejos.hardware.port.SensorPort;
+import lejos.robotics.chassis.Chassis;
+import lejos.robotics.chassis.Wheel;
+import lejos.robotics.chassis.WheeledChassis;
 
-public class Main
-{
-    public static void main(String[] args)
-    {
-//        System.out.println("Running...");
-        GraphicsLCD g = BrickFinder.getDefault().getGraphicsLCD();
-        final int SW = g.getWidth();
-        final int SH = g.getHeight();
-        Button.LEDPattern(4);
-        Sound.beepSequenceUp();
-        
-        g.setFont(Font.getLargeFont());
-        g.drawString("leJOS/EV3", SW/2, SH/2, GraphicsLCD.BASELINE|GraphicsLCD.HCENTER);
-        Button.LEDPattern(3);
-        Delay.msDelay(4000);
-        Button.LEDPattern(5);
-        g.clear();
-        g.refresh();
-        Sound.beepSequence();
-        Delay.msDelay(500);
-        Button.LEDPattern(0);
-        System.out.println("yes");
-    }
+public class Main {
+		
+	public static void main(String[] args ) throws Exception {
+				
+		Wheel wheel1 = WheeledChassis.modelWheel(Motor.B, 5.3).offset(-5.8);
+		Wheel wheel2 = WheeledChassis.modelWheel(Motor.C, 5.3).offset(5.8);
+		Chassis chassis = new WheeledChassis(new Wheel[] { wheel1, wheel2 }, WheeledChassis.TYPE_DIFFERENTIAL);
+		
+		MovePilot robot = new MovePilot(chassis);		
+		robot.setLinearAcceleration(2000);
+		robot.setLinearSpeed(100);  // cm/sec
+		robot.setAngularSpeed(90); // deg/sec
+		
+		EV3UltrasonicSensor sensor = new EV3UltrasonicSensor(SensorPort.S3);
+		sensor.enable();
+		
+//		Button.waitForAnyPress();
+	   
+//		robot.forward();
+//		while (getDist(sensor) > 0.2);
+//		robot.stop();
+//		while (getDist(sensor) < 0.40) {
+		for (int i=0;i<4;i++)	
+			robot.rotate(-360);
+//		}
+//		robot.stop();
+		
+		
+		sensor.close();
+	}
+	
+	static float getDist(EV3UltrasonicSensor sensor) {
+		SampleProvider res = sensor.getDistanceMode();
+		float [] sample = new float[res.sampleSize()];
+		res.fetchSample(sample, 0);
+		return sample[0];
+	}
 }
