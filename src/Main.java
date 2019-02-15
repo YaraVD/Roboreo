30e8ce212bf00fa0fe83560af9aa5f90c85a22b1
VincentPinet <vincent.pinet@gmail.com>
2019-02-15 11:17:45 +0100

    PalletsDetector

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,25 @@
+import lejos.hardware.Button;
 
 public class Main {
-		
+
 	public static void main(String[] args ) throws Exception {
-				
-		Robot robot = new Robot(); 
+
+		Robot robot = new Robot();
+		PalletsDetector palletsDetector = new PalletsDetector();
+		palletsDetector.start();
 		
-//		Button.waitForAnyPress();
+		System.out.println("Wait for any press to exit");
+		Button.waitForAnyPress();
+		System.out.println("Hum..");
 		
-		robot.claws.release();
-		robot.forward();
-		while(!robot.bumper.isPressed() && robot.ultrasonic.distance() >= 0.15);
-		robot.stop();
-		robot.claws.grab();
-		
+
+		// robot.claws.release();
+		// robot.forward();
+		// while(!robot.bumper.isPressed() && robot.ultrasonic.distance() >= 0.15);
+		// robot.stop();
+		// robot.claws.grab();
+
+		palletsDetector.end();
 		robot.close();
 	}
 }
